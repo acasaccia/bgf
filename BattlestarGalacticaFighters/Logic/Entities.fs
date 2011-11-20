f3d8fcab4c03e17f32824daebf0b38fea1214370
Andrea Casaccia <acasaccia@gmail.com>
2011-11-20 16:07:21 +0000

    Gestione input F#, entità di gioco

---
+++ BattlestarGalacticaFighters/Logic/Entities.fs
@@ -0,0 +1,58 @@
+﻿module Entities
+open Measures
+open Vector2D
+
+type Aircraft = 
+    {
+        Position : Vector2D<m>
+        Yaw : float<d>
+        Roll : float<d>
+        Shields : int
+    }
+
+type Raider =
+    {
+        Body : Aircraft
+    }
+
+type Viper = 
+    {
+        Body : Aircraft
+        Missiles : int
+        LockedTarget : Option<Raider>
+    }
+
+type Projectile = 
+    {
+        Position : Vector2D<m>
+        Velocity : Vector2D<m>
+    }
+
+type ViperProjectile = 
+    {
+        Body : Projectile
+    }
+
+type RaiderProjectile = 
+    {
+        Body : Projectile
+    }
+
+type Missile = 
+    {
+        Position : Vector2D<m>
+        Velocity : Vector2D<m/s>
+        Acceleration : Vector2D<m/s^2>
+        Yaw : float<d>
+    }
+
+type ViperMissile = 
+    {
+        Body : Missile
+        Target : Raider
+    }
+
+type RaiderMissile = 
+    {
+        Body : Missile
+    }
