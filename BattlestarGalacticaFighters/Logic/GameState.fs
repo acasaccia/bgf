9c909c447a90815628d26ceafdb9dc06ee09ff28
Andrea Casaccia <acasaccia@gmail.com>
2011-11-27 23:00:57 +0000

    Miglioramento gestione input

--- BattlestarGalacticaFighters/Logic/GameState.fs
+++ BattlestarGalacticaFighters/Logic/GameState.fs
@@ -1,5 +1,6 @@
 ﻿module GameState
 
+open System
 open Measures
 open Vector2D
 open Entities
@@ -8,28 +9,46 @@ open BattlestarGalacticaFightersInput
 
 type GameState =
  {
-  player : Variable<Viper>
+  viper : Variable<Viper>
  }
 
 let state : GameState =
  {
-  player = 
-    Variable( fun () -> { Position = Variable( fun () -> { X = 0.0<m>; Y = 0.0<m> } ) } )
+  viper = 
+    Variable( fun () -> 
+     { 
+      Position = Variable( fun () -> { X = 0.0f<m>; Y = 0.0f<m> } )
+      Yaw = Variable( fun () -> 0.0f<rad> )
+      Speed = 0.3f<m/s>
+     }
+    )
  }
+
+let rec update_state(dt:float32<s>) = 
+ update_viper !state.viper dt
+ state.viper := !state.viper
+ 
+and private update_viper (p:Viper) (dt:float32<s>) = 
+ p.Position := !p.Position +
+  if InputState.MoveLeft then
+   { X = p.Speed * dt; Y = 0.0f<m> }
+  elif InputState.MoveRight then
+   { X = -p.Speed * dt; Y = 0.0f<m> }
+  elif InputState.MoveUp then
+   { X = 0.0f<m>; Y = p.Speed * dt }
+  elif InputState.MoveDown then
+   { X = 0.0f<m>; Y = -p.Speed * dt }
+  else
+   { X = 0.0f<m>; Y = 0.0f<m> }
+ 
+ p.Yaw := 
+   if InputState.MoveLeft then
+    0.5f<rad>
+   elif InputState.MoveRight then
+    -0.5f<rad>
+   else
+     0.0f<rad>
 
 // Those should be used in scripts
 // let private (!) = immediate_read
 // let private (:=) = immediate_write
-
-let rec update_state(dt:float32) = 
- update_player !state.player dt
- state.player := !state.player
- 
-and private update_player (p:Viper) (dt:float32) = 
- p.Position := !p.Position +
-  if InputState.MoveLeft then
-   { X = -0.001<m>; Y = 0.0<m> }
-  elif InputState.MoveRight then
-   { X = 0.001<m>; Y = 0.0<m> }
-  else
-   { X = 0.0<m>; Y = 0.0<m> }
